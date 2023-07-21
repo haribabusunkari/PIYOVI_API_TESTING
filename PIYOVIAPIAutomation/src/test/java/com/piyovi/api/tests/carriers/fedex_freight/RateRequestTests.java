bd935930494d3b3d8641d996a977ca08ecd0681a
haribabu.sunkari@gmail.com <haribabu.sunkari@gmail.com>
2023-07-21 22:55:24 +0530

    FedEXFreight changes

--- PIYOVIAPIAutomation/src/test/java/com/piyovi/api/tests/carriers/fedex_freight/RateRequestTests.java
+++ PIYOVIAPIAutomation/src/test/java/com/piyovi/api/tests/carriers/fedex_freight/RateRequestTests.java
@@ -2,20 +2,25 @@ package com.piyovi.api.tests.carriers.fedex_freight;
 
 import com.piyovi.common.BasePage;
 import com.piyovi.constants.FedExFrieghtConstants;
+import com.piyovi.parsers.PiyoviResponseParser;
 import com.piyovi.util.FileHelper;
 import com.piyovi.util.JSONHelper;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class RateRequestTests extends BasePage {
-
+	PiyoviResponseParser responseParser = new PiyoviResponseParser();
+	
     @BeforeClass
     public void initialize() throws IOException {
         RestAssured.baseURI = propertyReader.getApplicationProperty("baseURI") + FedExFrieghtConstants.RATE_REQUEST_URL;
@@ -34,8 +39,10 @@ public class RateRequestTests extends BasePage {
                 .body(payload)
                 .when()
                 .post();
-
+        logger.info("Response Payload " + response.asPrettyString());
+        var responseMap = response.as(new TypeRef<Map<String, Object>>() {});
+        this.responseParser.parseResponse(responseMap);
+        assertTrue(this.responseParser.getRatesResp().size() > 0);
         assertEquals(response.statusCode(), 200);
-        // verifyTextAndLog("Verify Basic Rate Request", response.statusCode(),200);
     }
 }
