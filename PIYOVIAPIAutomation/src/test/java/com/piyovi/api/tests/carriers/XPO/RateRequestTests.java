98ddd5dd8c8c24c1c022ae920d4809670dcd2e20
haribabu.sunkari@gmail.com <haribabu.sunkari@gmail.com>
2023-11-26 13:39:11 +0530

    XPO Rate Request files

---
+++ PIYOVIAPIAutomation/src/test/java/com/piyovi/api/tests/carriers/XPO/RateRequestTests.java
@@ -0,0 +1,95 @@
+package com.piyovi.api.tests.carriers.XPO;
+
+import com.piyovi.common.BasePage;
+import com.piyovi.constants.XPOConstants;
+import com.piyovi.parsers.PiyoviResponseParser;
+import com.piyovi.util.DateTimeUtil;
+import com.piyovi.util.FileHelper;
+import com.piyovi.util.JSONHelper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class RateRequestTests extends BasePage {
+	DateTimeUtil dttmUtil = new DateTimeUtil();
+	String shipDtTmFormat = "yyyy-MM-dd'T'HH:mm:ssZ";
+	String shipDate = "";
+	PiyoviResponseParser responseParser = new PiyoviResponseParser();
+	
+    @BeforeClass
+    public void initialize() throws IOException {
+        RestAssured.baseURI = propertyReader.getApplicationProperty("baseURI") + XPOConstants.RATE_REQUEST_URL;
+        this.shipDate = dttmUtil.getDateTimeInFormat(this.shipDtTmFormat);
+    }
+
+    @Test
+    public void testRateRequestOK() {
+        logger = extent.createTest("XPO API - Test Basic Rate Request", "Verify Basic Rate Request");
+
+        var fileHelper = new FileHelper();
+        var payload = fileHelper.getFile(carriersPayLoadPath+XPOConstants.RATE_REQUEST_PAYLOAD);
+       
+        /* Update ship date and pickup date in the payload */
+        var jsonHelper = new JSONHelper();
+        payload = jsonHelper.updateJsonValue(payload, "ShipDate", this.shipDate);
+        payload = jsonHelper.updateJsonValue(payload, "$.SpecialServices.PickupDetails.PickupDate", this.shipDate);
+        logger.info("Request Payload " + payload);
+        
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post();
+        
+        /* Read required data from response */
+        logger.info("Response JSON " + response.asPrettyString());
+        int actualValue = Integer.parseInt(jsonHelper.getJsonValue(response.asPrettyString(), "$.status"));
+        logger.info("Actual Success: " + actualValue);
+        logger.info("Expected Success: 200");
+        
+        /* Compare actual vs expected */
+        assertEquals(actualValue,200);
+        
+        String data = jsonHelper.getJsonValue(response.asPrettyString(), "$.payload.Rates");
+        JSONArray array = new JSONArray(data);  
+        for(int index=0; index < array.length(); index++) {  
+	        //JSONObject object = array.getJSONObject(i);  
+	        //System.out.println(object.getString("CarrierName"));  
+	        //System.out.println(object.getString("ServiceName"));  
+        	String carrierName = jsonHelper.getJsonValue(response.asPrettyString(), "$.payload.Rates["+index+"].CarrierName");
+        	String serviceName = jsonHelper.getJsonValue(response.asPrettyString(), "$.payload.Rates["+index+"].ServiceName");
+        	String description = jsonHelper.getJsonValue(response.asPrettyString(), "$.payload.Rates["+index+"].ServiceDescription");
+        	
+        	/* Compare actual vs expected */
+        	logger.info("Actual Carrier Name: " + carrierName);
+            logger.info("Expected Carrier Name: XPOLogistics");
+            assertEquals(carrierName,"XPOLogistics");
+            
+            if(serviceName.toUpperCase().contains("STD")) {
+            	logger.info("Actual Service Name: " + serviceName);
+                logger.info("Expected Service Name: XPOSTD");
+            	assertEquals(serviceName,"XPOSTD");
+            	
+            	logger.info("Actual Service Description: " + description);
+                logger.info("Expected Service Description: Standard");
+            	assertEquals(description,"Standard");
+            }
+            else if (serviceName.toUpperCase().contains("GSS5")) {
+            	logger.info("Actual Service Name: " + serviceName);
+                logger.info("Expected Service Name: XPOGSS5");
+            	assertEquals(serviceName,"XPOGSS5");
+            	
+            	logger.info("Actual Service Description: " + description);
+                logger.info("Expected Service Description: Guaranteed by 5 PM");
+            	assertEquals(description,"Guaranteed by 5 PM");
+            }
+        }  
+    }
+}
